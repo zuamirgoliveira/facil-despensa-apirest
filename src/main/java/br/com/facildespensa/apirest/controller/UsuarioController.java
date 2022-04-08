f99e42fed4b47595ece7d16974862bc31a2c66d0
zuamirgoliveira <zuamir.g.oliveira@accenture.com>
2022-04-08 00:02:58 -0300

    feat[] Implementação do swagger para documentação

--- src/main/java/br/com/facildespensa/apirest/controller/UsuarioController.java
+++ src/main/java/br/com/facildespensa/apirest/controller/UsuarioController.java
@@ -3,7 +3,10 @@ package br.com.facildespensa.apirest.controller;
 import br.com.facildespensa.apirest.model.Usuario;
 import br.com.facildespensa.apirest.repository.DespensaRepository;
 import br.com.facildespensa.apirest.repository.UsuarioRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/apirest")
+@RequestMapping(value = "/api")
+@Api(value = "API REST Usuários")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @Autowired
-    DespensaRepository despensaRepository;
-
-
     @GetMapping(value = "/usuarios")
+    @ApiOperation(value = "Retorna uma lista de usuários")
     public List<Usuario> listarUsuarios() {
         List<Usuario> usuarios = usuarioRepository.findAll();
         return usuarios;
